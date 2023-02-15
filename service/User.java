f9b08a5fbec553c92c103bb7172054c411ba6de3
chrismariano <christopherj.mariano@gmail.com>
2023-02-15 12:13:08 -0500

    ANC-2 Add User.admin javadoc

--- service/User.java
+++ service/User.java
@@ -2,6 +2,9 @@ package service;
 
 
 public class User {
+    /**
+     * @returns <code>true</code> if user is admin, otherwise <code>false</code>
+     */
     public boolean isAdmin;
 
     public Object getId() {
